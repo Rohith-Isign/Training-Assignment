9f09091e0f084e223e439946570803dce87609aa
rohith-isign <rohith.mamidala@isigntech.com>
2024-06-28 10:26:13 +0530

    StreamAPI Updates

---
+++ streamAPI/src/streamAPI/ParallelProcessingEx.java
@@ -0,0 +1,16 @@
+package streamAPI;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class ParallelProcessingEx {
+
+	public static void main(String[] args) {
+		List<Integer> preOpe = IntStream.range(0, 1000000).boxed().collect(Collectors.toList());
+		long startTime = System.nanoTime();
+		List<Integer> postOpe = preOpe.stream().map(e->e+e).sorted().filter(e->e%2==0).collect(Collectors.toList());
+		long endTime = System.nanoTime();
+		System.out.println("Processing time of Sequential Streams : "+ (endTime-startTime));
+	}
+}
