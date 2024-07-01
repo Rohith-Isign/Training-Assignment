4526ce7e8d2f09ddf328b9fa4c9dfcbf18f3d7ba
rohith-isign <rohith.mamidala@isigntech.com>
2024-07-01 11:04:59 +0530

    updates_IO_Sream

---
+++ streamAPI/src/io_stream/IO_Stream_ASCII.java
@@ -0,0 +1,29 @@
+package io_stream;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
+
+public class IO_Stream_ASCII {
+
+	 public static void main(String[] args) throws IOException {
+	        InputStream fileInputStream = new FileInputStream("src/io_stream/MyFileASCII");
+
+	        Reader inputStreamReader = new InputStreamReader(fileInputStream, StandardCharsets.US_ASCII);
+
+	        int fileData = inputStreamReader.read();
+
+	        System.out.println("File-1 Data...\n");
+
+	        while (fileData != -1) {
+	            System.out.println("fileData ASCII: " + fileData + ", fileData char: " + (char) fileData);
+
+	            fileData = inputStreamReader.read();
+	        }
+
+	        inputStreamReader.close();
+	    }
+}
--- streamAPI/src/io_stream/IO_Stream_UTF.java
+++ streamAPI/src/io_stream/IO_Stream_UTF.java
@@ -7,15 +7,16 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.nio.charset.Charset;
 
-public class IO_Stream_Ex_2 {
+public class IO_Stream_UTF {
 	public static void main(String[] args) throws IOException {
 		InputStream fileInputStream2 = new FileInputStream("src/io_stream/myFile2");
 		Reader inputStreamReader2 = new InputStreamReader(fileInputStream2, Charset.forName("UTF-8"));
 		int fileData2 = inputStreamReader2.read();
 		System.out.println("\nFile-1 Data...\n");
 		while (fileData2 != -1) {
-			System.out.println("fileData encode value : " + fileData2 + ", filedata char : " + (char) fileData2);
+			System.out.println("fileData integer representation : " + fileData2 + ", filedata char : " + (char) fileData2);
 			fileData2 = inputStreamReader2.read();
 		}
+		inputStreamReader2.close();
 	}
 }
